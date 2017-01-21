86c4a3833e4fc0e79013520c81f1a49e6ef57079
Joël Wasmer <morbrolhc@gmail.com>
2017-01-21 11:26:21 +0100

    Hi

---
+++ GlobalGameJam2017/Assets/Scripts/Spells/PillarSpell.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillarSpell : Spell {
+
+    public Transform spellIndicator;
+    public float minRadius = 3;
+    public float maxRadius = 6;
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        if (IsChanneling)
+        {
+            var distance = ((Time.time - ChannelingStartTime) / maxChannelingTime) * maxCastRange;
+            var radius = ((Time.time - ChannelingStartTime) / maxChannelingTime) * (maxRadius - minRadius) + minRadius;
+            spellIndicator.localPosition = new Vector3(0f, 0f, distance);
+            spellIndicator.localScale = new Vector3(radius, spellIndicator.localScale.y, radius);
+        }
+        else
+        {
+            spellIndicator.localPosition = Vector3.zero;
+            spellIndicator.localScale = new Vector3(1e-5f, spellIndicator.localScale.y, 1e-5f);
+        }
+    }
+}
