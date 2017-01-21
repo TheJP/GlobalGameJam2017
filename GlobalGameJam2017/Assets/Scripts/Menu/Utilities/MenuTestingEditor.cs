791695a97651511d7cbd5c26159e5f739a46daa5
Ruben Nunez <ruben.nunez@live.de>
2017-01-21 16:34:27 +0100

    Menu 80% finished

---
+++ GlobalGameJam2017/Assets/Scripts/Menu/Utilities/MenuTestingEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts.Menu
+{
+    [CustomEditor(typeof(MenuScreen))]
+    public class EditorScreenTesting : Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            var screen = (MenuScreen)target;
+            if (GUILayout.Button("Slide Out right"))
+            {
+                screen.SlideOut(SlideDirection.right);
+            }
+            if (GUILayout.Button("Slide Out left"))
+            {
+                screen.SlideOut(SlideDirection.left);
+            }
+            if (GUILayout.Button("Slide Out top"))
+            {
+                screen.SlideOut(SlideDirection.top);
+            }
+            if (GUILayout.Button("Slide In"))
+            {
+                screen.SlideIn();
+            }
+        }
+    }
+}
