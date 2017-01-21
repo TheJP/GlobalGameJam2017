08007bc0fb34ba4b72d0c875c27ef379deb8c364
TheJP <jp_55555@hotmail.com>
2017-01-21 06:51:35 +0100

    Added golem animation and rotation to walking direction

--- GlobalGameJam2017/Assets/Scripts/Characters/Player.cs
+++ GlobalGameJam2017/Assets/Scripts/Characters/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Assertions;
 
 [RequireComponent(typeof(NavMeshAgent))]
 [RequireComponent(typeof(Rigidbody))]
@@ -9,21 +10,32 @@ public class Player : Entity {
 
     public string playerName = "A";
     public float speed = 0.1f;
+    public Animator animator;
+
+    private const string SlamTrigger = "Slam";
 
     protected override void Start () {
         base.Start();
         base.EntityFaction = Faction.PC;
+        Assert.IsNotNull(animator, "Need golem animator in Player");
 	}
 
     protected override void FixedUpdate () {
         base.FixedUpdate();
-        if (Health > 0.0f) { Move(); }
+        if (Health > 0.0f)
+        {
+            Move();
+            if (Input.GetButton(playerName + "_a")) { animator.SetTrigger(SlamTrigger); }
+        }
     }
 
     private void Move()
     {
-        Vector3 v = new Vector3(Input.GetAxisRaw(playerName + "_Horizontal"), 0.0f, Input.GetAxisRaw(playerName + "_Vertical"));
-        GetComponent<NavMeshAgent>().Move(v.normalized * speed);
+        Vector3 v = new Vector3(Input.GetAxisRaw(playerName + "_Horizontal"), 0.0f, Input.GetAxisRaw(playerName + "_Vertical")).normalized;
+        var isWalking = v.sqrMagnitude > 0.5f;
+        animator.SetBool("Walking", isWalking);
+        if (isWalking) { transform.rotation = Quaternion.LookRotation(v); }
+        GetComponent<NavMeshAgent>().Move(v * speed);
     }
 
 }
