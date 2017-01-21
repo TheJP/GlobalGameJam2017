25bce8501d122e94ab62290e3aebdc009b97d261
TheJP <jp_55555@hotmail.com>
2017-01-21 15:08:14 +0100

    Made enemies more physic dependend (nav mesh can't save them anymore)

--- GlobalGameJam2017/Assets/Scripts/Characters/Enemy.cs
+++ GlobalGameJam2017/Assets/Scripts/Characters/Enemy.cs
@@ -12,6 +12,10 @@ public class Enemy : Entity
     public int attackRange;
     public int attackSpeed;
     public GameObject playersGroup;
+    [Tooltip("Physical force which the enemy can use to walk")]
+    public float walkingForce = 1.0f;
+    public float fallingForce = 100.0f;
+
     private Player agro = null;
     private bool attacking = false;
     private int tick = 0;
@@ -20,28 +24,32 @@ public class Enemy : Entity
     {
         base.Start();
         base.EntityFaction = Faction.NPC;
-	}
+        var agent = GetComponent<NavMeshAgent>();
+        agent.updatePosition = false;
+        //agent.updateRotation = false;
+    }
 
     protected override void FixedUpdate()
     {
         base.FixedUpdate();
-        if (agro == null || agro.Health <= 0.0f) { AttackNearestPlayer(); }
-        else { GetComponent<NavMeshAgent>().SetDestination(agro.transform.position); }
-        if (agro != null && Vector3.Distance(transform.position, agro.transform.position) < attackRange)
-        {
-            attacking = true;
-        } else
-        {
-            attacking = false;
-            tick = 0;
-        }
-        if(attacking)
-        {
-            Attack();
-        }
+        var agent = GetComponent<NavMeshAgent>();
+        agent.nextPosition = transform.position;
+
+        //Navigation
+        if (agro == null || agro.Health <= 0.0f) { GoToNearestPlayer(); }
+        else { agent.SetDestination(agro.transform.position); }
+
+        //Handle walking force
+        var force = agent.desiredVelocity.normalized * walkingForce;
+        GetComponent<Rigidbody>().AddForce(new Vector3(force.x, -fallingForce, force.z));
+
+        //Attacking
+        attacking = agro != null && Vector3.Distance(transform.position, agro.transform.position) < attackRange;
+        if (attacking) { Attack(); }
+        else { tick = 0; }
     }
 
-    private void AttackNearestPlayer()
+    private void GoToNearestPlayer()
     {
         //Search nearest player on nav mesh
         NavMeshPath path;
@@ -62,7 +70,7 @@ public class Enemy : Entity
             }
         }
 
-        //Attack nearest player
+        //Go to nearest player
         if (bestPath != null)
         {
             agent.SetPath(bestPath);
@@ -72,7 +80,7 @@ public class Enemy : Entity
 
     private void Attack()
     {
-        if(tick == attackSpeed)
+        if (tick == attackSpeed)
         {
             agro.DoDamage(this.Damage);
             tick = 0;
