67a009a2aa3b8157a61017cf70668953325b1c83
Ruben Nunez <ruben.nunez@live.de>
2017-01-28 20:35:01 +0100

    real Waves on water mesh

---
+++ GlobalGameJam2017/Assets/Materials/Water/WaterGen.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterGen : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+
+    public float scale = 10.0f;
+    public float speed = 1.0f;
+    public float noiseStrength = 4.0f;
+    public float noiseWalk = 1f;
+
+    private Vector3[] baseHeight;
+
+    void Update()
+    {
+        var mesh = GetComponent<MeshFilter>().mesh;
+
+        if (baseHeight == null)
+            baseHeight = mesh.vertices;
+
+        var vertices = new Vector3[baseHeight.Length];
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            var vertex = baseHeight[i];
+            vertex.y += Mathf.Sin(Time.time * speed + baseHeight[i].x + baseHeight[i].y + baseHeight[i].z) * scale;
+            vertex.y += Mathf.PerlinNoise(baseHeight[i].x + noiseWalk, baseHeight[i].y + Mathf.Sin(Time.time * 0.1f)) * noiseStrength;
+            vertices[i] = vertex;
+        }
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+    }
+}
